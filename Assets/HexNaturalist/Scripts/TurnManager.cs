7af3dd46c2e6ea538ad35a1b57d452ec67f6ca1f
jonyfries <jonyfries@hotmail.com>
2019-01-05 05:48:07 -0500

    End turn button only visible during player turn.

--- Assets/HexNaturalist/Scripts/TurnManager.cs
+++ Assets/HexNaturalist/Scripts/TurnManager.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class TurnManager : Singleton<TurnManager>
 {
+    public GameObject endTurnButton;
+    public CharacterController player;
+
     List<CharacterController> characterList = new List<CharacterController>();
 
+    /// <summary>
+    /// Setup the turn manager
+    /// </summary>
     void Start()
     {
         Invoke("LateStart", .12f);
+        endTurnButton.SetActive(false);
     }
 
+    /// <summary>
+    /// Start the first character's turn
+    /// </summary>
     void LateStart()
     {
-        characterList[0].StartTurn();
+        StartTurn(characterList[0]);
     }
 
-    public void SubscribeCharacter(CharacterController character)
-    {
-        characterList.Add(character);
-    }
-
+    /// <summary>
+    /// Remove character from the turn order.
+    /// </summary>
+    /// <param name="character">Character to remove.</param>
     public void DestroyCharacter(CharacterController character)
     {
         characterList.Remove(character);
     }
 
+    /// <summary>
+    /// End the current character's turn and start the next turn.
+    /// </summary>
+    /// <param name="character">The character's turn which is ending.</param>
     public void EndTurn(CharacterController character)
     {
         if (character != characterList[0])
@@ -36,6 +49,25 @@ public class TurnManager : Singleton<TurnManager>
         CharacterController temp = characterList[0];
         characterList.RemoveAt(0);
         characterList.Add(temp);
-        characterList[0].StartTurn();
+        StartTurn(characterList[0]);
+    }
+
+    /// <summary>
+    /// Start a character's turn
+    /// </summary>
+    /// <param name="character">The character who's turn is starting</param>
+    private void StartTurn(CharacterController character)
+    {
+        character.StartTurn();
+        endTurnButton.SetActive(character == player);
+    }
+
+    /// <summary>
+    /// Add a character to the turn order.
+    /// </summary>
+    /// <param name="character">Character to add.</param>
+    public void SubscribeCharacter(CharacterController character)
+    {
+        characterList.Add(character);
     }
 }
